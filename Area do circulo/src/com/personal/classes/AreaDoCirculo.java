e39eec1fc3746290aadeb47fa991eeb62b484c9f
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-05-12 15:47:57 -0400

    Código final

--- Area do circulo/src/com/personal/classes/AreaDoCirculo.java
+++ Area do circulo/src/com/personal/classes/AreaDoCirculo.java
@@ -1,10 +1,18 @@
 package com.personal.classes;
 
+import java.util.Scanner;
+
 public class AreaDoCirculo {
 
 	public static void main(String[] args) {
 		
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Para calcular a área do círculo, informe o valor do raio em centímetros: ");
 		
+		double raio = scanner.nextDouble();
+		double area = 3.14 * (Math.pow(raio, 2));
+		
+		System.out.println("A área do círculo é: " + area + "cm²");
 		
 	}
 
